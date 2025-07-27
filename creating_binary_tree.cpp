de702eca0c32f00c79348f529dd61ba2d5760bc5
MashrafiGit <mdmasrafi902@gmail.com>
2025-07-27 13:13:16 +0600

    creating binary tree

---
+++ creating_binary_tree.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class BiNode
+{
+public:
+    int val;
+    BiNode *left;
+    BiNode *right;
+
+    BiNode(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+int main()
+{
+
+    BiNode *root = new BiNode(10);
+    BiNode *a = new BiNode(20);
+    BiNode *b = new BiNode(30);
+    BiNode *c = new BiNode(40);
+    BiNode *d = new BiNode(50);
+    BiNode *e = new BiNode(60);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    b->left = d;
+    b->right = e;
+
+    return 0;
+}
