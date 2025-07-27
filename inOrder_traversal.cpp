22e5df98af8be85cb9f6b99b9d6613792e9f8a1c
MashrafiGit <mdmasrafi902@gmail.com>
2025-07-27 13:16:33 +0600

    binary tree in order traversal

---
+++ inOrder_traversal.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class BiNode
+{
+public:
+    int val;
+    BiNode *left;
+    BiNode *right;
+
+    BiNode(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void inOrder(BiNode *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inOrder(root->left);
+    cout << root->val << " ";
+    inOrder(root->right);
+}
+
+int main()
+{
+
+    BiNode *root = new BiNode(10);
+    BiNode *a = new BiNode(20);
+    BiNode *b = new BiNode(30);
+    BiNode *c = new BiNode(40);
+    BiNode *d = new BiNode(50);
+    BiNode *e = new BiNode(60);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    b->left = d;
+    b->right = e;
+
+    inOrder(root);
+
+    return 0;
+}
